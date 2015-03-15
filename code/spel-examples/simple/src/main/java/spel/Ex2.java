938c49113888bd427327ebddf87a46c748a4f13c
Thomas Darimont <thomas.darimont@gmail.com>
2015-03-15 20:40:47 +0100

    Use a more complex expression in the example.

--- code/spel-examples/simple/src/main/java/spel/Ex2.java
+++ code/spel-examples/simple/src/main/java/spel/Ex2.java
@@ -1,10 +1,15 @@
 package spel;
+
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.EvaluationException;
 import org.springframework.expression.spel.CompiledExpression;
-public class Ex2 extends CompiledExpression
-{
-  public Object getValue(Object paramObject, EvaluationContext paramEvaluationContext)
-    throws EvaluationException
-  { return ((String)paramObject).toUpperCase(); }
+
+import java.time.LocalDate;
+
+public class Ex2
+        extends CompiledExpression {
+    public Object getValue(Object paramObject, EvaluationContext paramEvaluationContext)
+            throws EvaluationException {
+        return Integer.valueOf(((LocalDate) paramObject).getYear() - ((LocalDate) paramEvaluationContext.lookupVariable("dobTesla")).getYear());
+    }
 }
--- code/spel-examples/simple/src/test/java/SimpleSpelExpressionExampleTests.java
+++ code/spel-examples/simple/src/test/java/SimpleSpelExpressionExampleTests.java
@@ -1,8 +1,12 @@
 import org.junit.Test;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.spel.SpelCompilerMode;
 import org.springframework.expression.spel.SpelParserConfiguration;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.time.LocalDate;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -10,7 +14,7 @@ import static org.junit.Assert.assertThat;
 public class SimpleSpelExpressionExampleTests {
 
     /**
-     * @see EXAMPLE-1
+     * @see EXAMPLE-0
      */
     @Test
     public void parseAndExecuteSimpleSpelExpression() {
@@ -25,5 +29,26 @@ public class SimpleSpelExpressionExampleTests {
         String result = expression.getValue("Nikola Tesla", String.class);
 
         assertThat(result, is("NIKOLA TESLA"));
+    }
+
+    /**
+     * @see EXAMPLE-1
+     */
+    @Test
+    public void parseAndExecuteSpelExpressionWithVariables() {
+
+        SpelParserConfiguration config = new SpelParserConfiguration(SpelCompilerMode.IMMEDIATE, getClass().getClassLoader());
+        SpelExpressionParser parser = new SpelExpressionParser(config);
+
+        EvaluationContext context = new StandardEvaluationContext();
+        context.setVariable("dobTesla", LocalDate.of(1856, 6, 10));
+
+        Expression expression = parser.parseExpression("getYear() - #dobTesla.getYear()");
+
+        Integer result = expression.getValue(context, LocalDate.of(2015, 3, 15), Integer.class);
+
+        //trigger compilation
+        result = expression.getValue(context, LocalDate.of(2015, 3, 15), Integer.class);
+        assertThat(result, is(159));
     }
 }
